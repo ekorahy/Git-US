ddc5d59bf464e79fb69cefc37bcb1ab5e601bb98
ekorahy <ekorahy@gmail.com>
2024-02-22 18:33:19 +0700

    delete unnedded icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionName = "1.0"
         buildConfigField("String", "BASE_URL", "\"https://api.github.com/\"")
         buildConfigField("String", "GITHUB_TOKEN",
-            "\"{GITHUB_TOKEN}\""
+            "\"{GITHUB_TOKEN}}\""
         )
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
